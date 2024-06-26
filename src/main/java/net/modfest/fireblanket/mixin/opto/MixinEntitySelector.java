1f60ccd997cc47b42059eb0de871ffdbfc0277af
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2024-06-25 21:02:55 -0400

    Fix entity selector crash

--- src/main/java/net/modfest/fireblanket/mixin/opto/MixinEntitySelector.java
+++ src/main/java/net/modfest/fireblanket/mixin/opto/MixinEntitySelector.java
@@ -2,11 +2,14 @@ package net.modfest.fireblanket.mixin.opto;
 
 import com.google.common.collect.Lists;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.command.EntitySelector;
 import net.minecraft.entity.Entity;
 import net.minecraft.predicate.NumberRange;
+import net.minecraft.resource.featuretoggle.FeatureSet;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import org.jetbrains.annotations.Nullable;
@@ -15,6 +18,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
@@ -31,7 +35,7 @@ public abstract class MixinEntitySelector {
 
 	@Shadow @Final private Function<Vec3d, Vec3d> positionOffset;
 
-	@Shadow protected abstract Predicate<Entity> getPositionPredicate(Vec3d pos);
+	@Shadow protected abstract Predicate<Entity> getPositionPredicate(Vec3d pos, @Nullable Box box, @Nullable FeatureSet enabledFeatures);
 
 	@Shadow @Final private boolean senderOnly;
 
@@ -41,11 +45,13 @@ public abstract class MixinEntitySelector {
 
 	@Shadow protected abstract <T extends Entity> List<T> getEntities(Vec3d pos, List<T> entities);
 
-	@Shadow @Final private Predicate<Entity> basePredicate;
-
 	@Shadow @Final private NumberRange.DoubleRange distance;
 
 	@Shadow @Final private @Nullable Box box;
+
+	@Shadow @Nullable protected abstract Box getOffsetBox(Vec3d offset);
+
+	@Shadow @Final private List<Predicate<Entity>> predicates;
 
 	/**
 	 * @author Jasmine
@@ -62,7 +68,8 @@ public abstract class MixinEntitySelector {
 			return serverPlayerEntity == null ? Collections.emptyList() : Lists.<ServerPlayerEntity>newArrayList(serverPlayerEntity);
 		} else {
 			Vec3d pos = this.positionOffset.apply(source.getPosition());
-			Predicate<Entity> predicate = this.getPositionPredicate(pos);
+			Box box = this.getOffsetBox(pos);
+			Predicate<Entity> predicate = this.getPositionPredicate(pos, box, null);
 			if (this.senderOnly) {
 				if (source.getEntity() instanceof ServerPlayerEntity serverPlayerEntity2 && predicate.test(serverPlayerEntity2)) {
 					return Lists.newArrayList(serverPlayerEntity2);
@@ -74,10 +81,12 @@ public abstract class MixinEntitySelector {
 				List<ServerPlayerEntity> list;
 				if (this.isLocalWorldOnly()) {
 					// The change is here: Get players with distance predicate first, move onto base predicate later.
-					predicate = getPositionOnlyPredicate(pos);
+					predicate = getPositionOnlyPredicate(pos, box, null);
+
+					Predicate<Entity> basePredicate = Util.allOf(this.predicates);
 
 					list = source.getWorld().getPlayers(predicate, i);
-					list.removeIf(this.basePredicate.negate());
+					list.removeIf(basePredicate.negate());
 				} else {
 					list = Lists.newArrayList();
 
@@ -108,5 +117,33 @@ public abstract class MixinEntitySelector {
 		}
 
 		return predicate;
+	}
+
+	private Predicate<Entity> getPositionOnlyPredicate(Vec3d pos, @Nullable Box box, @Nullable FeatureSet enabledFeatures) {
+		boolean bl = enabledFeatures != null;
+		boolean bl2 = box != null;
+		boolean bl3 = !this.distance.isDummy();
+		int i = (bl ? 1 : 0) + (bl2 ? 1 : 0) + (bl3 ? 1 : 0);
+		List<Predicate<Entity>> list;
+		if (i == 0) {
+			list = new ArrayList<>();
+		} else {
+			List<Predicate<Entity>> list2 = new ObjectArrayList<>(3);
+			if (bl) {
+				list2.add(entity -> entity.getType().isEnabled(enabledFeatures));
+			}
+
+			if (bl2) {
+				list2.add(entity -> box.intersects(entity.getBoundingBox()));
+			}
+
+			if (bl3) {
+				list2.add(entity -> this.distance.testSqrt(entity.squaredDistanceTo(pos)));
+			}
+
+			list = list2;
+		}
+
+		return Util.allOf(list);
 	}
 }
